36765ca80ded4b3be08cb5b0c89a24dbeac3a991
samk257 <kubuntusampras.sk@gmail.com>
2023-09-30 06:13:08 +0200

    working with role seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +15,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $this->call([RoleSeeder::class]);
+        User::create([
+            'name'=>"CLEB",
+            'email'=>"admin@cleb.me",
+            'username'=>"cleb_admin",
+            'telephone'=>"68 18 40 40",
+            'address'=>"Kinindo, Bujumbura",
+            'password'=>Hash::make('12345678'),
+            'role_id'=>1
+         ]);
     }
 }
